66582e174101d758e687637ce0c4fc0d56a5eec1
Juan Pablo Balda <jpbalda@users.noreply.github.com>
2024-06-16 20:07:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  console.log("My skills include: ");
+  console.log("1. JavaScript");
+  console.log("2. HTML");
+  console.log("3. CSS");
+  console.log("4. React");
+  console.log("5. Node.js");
+}
